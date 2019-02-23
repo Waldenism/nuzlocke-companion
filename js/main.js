14490aaeb7a22dccd91e547dee1911d0c4603772
Greggles <gregorynimmo@hotmail.com>
2019-02-23 15:20:55 +0100

    Add responsive navigation bar with place holder menu items

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function myFunction() {
+  var x = document.getElementById("navigation");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+
+  var y = document.getElementById("nav-collapse");
+  if(y.className === "nav-right") {
+     y.className = "nav-left"
+
+  }else{
+    y.className = y.className.replace(/\bmystyle\b/g, "");
+    y.className = "nav-right"
+  }
+}
