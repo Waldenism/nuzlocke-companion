6b1456d63a243980ac804a78924b3de15614bba0
Chrisplaysharp <cwalden@berkeley.edu>
2019-02-24 20:48:27 -0500

    sticky header and some packages

--- js/main.js
+++ js/main.js
@@ -1,16 +1,33 @@
-function myFunction() {
-  var x = document.getElementById("navigation");
-  if (x.className === "navbar") {
-    x.className += " responsive";
+// When the user scrolls the page, execute stickyHeader() 
+window.onscroll = function() {stickyHeader()}
+
+function navFunction () {
+  var x = document.getElementById('navigation')
+  if (x.className === 'navbar') {
+    x.className += ' responsive'
   } else {
-    x.className = "navbar";
+    x.className = 'navbar'
   }
 
-  var y = document.getElementById("nav-collapse");
-  if(y.className === "nav-right") {
-     y.className = "nav-left"
-
-  }else{
-    y.className = "nav-right"
+  var y = document.getElementById('nav-collapse')
+  if (y.className === 'nav-right') {
+    y.className = 'nav-left'
+  } else {
+    y.className = 'nav-right'
   }
 }
+
+function stickyHeader() {
+  let nav = document.getElementById('navigation')
+  // get offset position of navbar
+  let sticky = nav.offsetTop
+
+
+  if (window.pageYOffset > sticky) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+}
+
+
